4d71cfddd5472b24c6760233c018914a1ab38200
Ahmed Yasser <ayasser@softmills.com>
2021-11-28 16:09:22 +0200

    Added Dialogs

--- lib/Screens/More/settings_screen.dart
+++ lib/Screens/More/settings_screen.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
@@ -52,12 +53,14 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 OptionRowWidget(
                   image: R.images.notificationBellImage,
                   title: "Notifications & Sounds",
-                  onPressed: () => Navigator.pushNamed(context, R.routes.notificationsSoundsRoute),
+                  onPressed: () => Navigator.pushNamed(
+                      context, R.routes.notificationsSoundsRoute),
                 ),
                 OptionRowWidget(
                   image: R.images.chatImage,
                   title: "Chat Settings",
-                  onPressed: () => Navigator.pushNamed(context, R.routes.chatSettingsRoute),
+                  onPressed: () =>
+                      Navigator.pushNamed(context, R.routes.chatSettingsRoute),
                 ),
                 OptionRowWidget(
                   image: R.images.blockedUserImage,
@@ -67,12 +70,15 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 OptionRowWidget(
                   image: R.images.languageImage,
                   title: "Language",
-                  onPressed: () => Navigator.pushNamed(context, R.routes.languageRoute),
+                  onPressed: () =>
+                      Navigator.pushNamed(context, R.routes.languageRoute),
                 ),
                 const SizedBox(height: 5),
                 buildSettingsChoices("Help", () {},
                     bottom: 30, textColor: const Color(0xff232323)),
-                buildSettingsChoices("Ask a Question", () {}),
+                buildSettingsChoices("Ask a Question", () {
+                  _showAskQuestionDialog();
+                }),
                 buildSettingsChoices(
                   "Terms & Conditions",
                   () =>
@@ -114,6 +120,103 @@ class _SettingsScreenState extends State<SettingsScreen> {
           ),
         ).tr(),
       ),
+    );
+  }
+
+  Future<void> _showAskQuestionDialog() {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.white,
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              side: BorderSide(color: Colors.white, width: 1.0)),
+          title: Center(
+            child: Text(
+              "Ask a Question".tr(),
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          content: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  "Please note that our APP support is done by volunteers. We try to respond as quickly as possible,but it may take a while.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.grey.shade600),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 5, bottom: 5),
+                          child: Text(
+                            "Cancel",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Colors.grey.shade600, fontSize: 16),
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.transparent,
+                          elevation: 0,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            side: BorderSide(
+                              color: Colors.grey.shade600,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 5, bottom: 5),
+                          child: Text(
+                            "Ask a Volunteer",
+                            textAlign: TextAlign.center,
+                            maxLines: 1,
+                            style: TextStyle(
+                                color: Theme.of(context).primaryColorDark,
+                                fontSize: 15),
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.transparent,
+                          elevation: 0,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            side: BorderSide(
+                                color: Theme.of(context).primaryColorDark),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
