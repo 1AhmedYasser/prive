3ad918339ea2e686b0ad05441b51d7998218736b
Ahmed Yasser <ayasser@softmills.com>
2022-04-27 12:06:05 +0200

    Added Rooms Screens

---
+++ lib/Screens/Rooms/people_chooser_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PeopleChooserScreen extends StatefulWidget {
+  const PeopleChooserScreen({Key? key}) : super(key: key);
+
+  @override
+  State<PeopleChooserScreen> createState() => _PeopleChooserScreenState();
+}
+
+class _PeopleChooserScreenState extends State<PeopleChooserScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
