d525b9c2c0e6441a8e72c1edc1bf8dba71e443a1
Ahmed Yasser <ayasser@softmills.com>
2022-05-02 07:01:28 +0200

    Updated Stream

--- lib/Extras/resources.dart
+++ lib/Extras/resources.dart
@@ -146,7 +146,7 @@ class SharedPref {
 }
 
 class Constants {
-  String streamKey = 'ymyds4cvwdx6';
+  String streamKey = '5agc7xsw7tyj';
   String agoraAppId = "666a21c863d9431da1ee9651748608fb";
 }
 
